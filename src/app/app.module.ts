8290a14ec36cb71e773a1b79cf995772850269bb
Sanket <sanketlandge568@gmail.com>
2021-08-16 12:36:52 +0530

    initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherWidgetMainComponent } from './Compoents/weather-widget-main/weather-widget-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetMainComponent
   ],
   imports: [
     BrowserModule
